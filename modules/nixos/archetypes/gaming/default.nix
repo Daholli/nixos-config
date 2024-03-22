fcaa48df900d57d0d304cfc95bd798bf7217b4f0
christoph.hollizeck <christoph.hollizeck@web.de>
2024-03-22 18:18:58 +0100

    to be continued

---
+++ modules/nixos/archetypes/gaming/default.nix
@@ -0,0 +1,21 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.wyrdgard;
+let
+  cfg = config.wyrdgard.archetypes.gaming;
+in
+{
+  options.wyrdgard.archetypes.gaming = with types; {
+    enable = mkBoolOpt false "Whether or not to enable the gaming archetype.";
+  };
+
+  config = mkIf cfg.enable {
+    wyrdgard.submodules = {
+      basics = enabled;
+      graphical-interface = enabled;
+      games = enabled;
+      socials = enabled;
+    };
+  };
+}
---
+++ modules/nixos/user/default.nix
@@ -0,0 +1,112 @@
+{ options
+, config
+, pkgs
+, lib
+, ...
+}:
+with lib;
+with lib.wyrdgard; let
+  cfg = config.wyrdgard.user;
+  defaultIconFileName = "profile.png";
+  defaultIcon = pkgs.stdenvNoCC.mkDerivation {
+    name = "default-icon";
+    src = ./. + "/${defaultIconFileName}";
+
+    dontUnpack = true;
+
+    installPhase = ''
+      cp $src $out
+    '';
+
+    passthru = { fileName = defaultIconFileName; };
+  };
+  propagatedIcon =
+    pkgs.runCommandNoCC "propagated-icon"
+      { passthru = { fileName = cfg.icon.fileName; }; }
+      ''
+        local target="$out/share/wyrdgard-icons/user/${cfg.name}"
+        mkdir -p "$target"
+
+        cp ${cfg.icon} "$target/${cfg.icon.fileName}"
+      '';
+in
+{
+  options.wyrdgard.user = with types; {
+    name = mkOpt str "short" "The name to use for the user account.";
+    fullName = mkOpt str "Christoph Hollizeck" "The full name of the user.";
+    email = mkOpt str "christoph.hollizeck@hey.com" "The email of the user.";
+    initialPassword =
+      mkOpt str "asdf"
+        "The initial password to use when the user is first created.";
+    icon =
+      mkOpt (nullOr package) defaultIcon
+        "The profile picture to use for the user.";
+    extraGroups = mkOpt (listOf str) [ ] "Groups for the user to be assigned.";
+    extraOptions =
+      mkOpt attrs { }
+        (mdDoc "Extra options passed to `users.users.<name>`.");
+  };
+
+  config = {
+    environment.systemPackages = with pkgs; [
+
+    ];
+
+    programs.zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      histFile = "$XDG_CACHE_HOME/zsh.history";
+    };
+
+    wyrdgard.home = {
+      file = {
+        "Desktop/.keep".text = "";
+        "Documents/.keep".text = "";
+        "Downloads/.keep".text = "";
+        "Music/.keep".text = "";
+        "Pictures/.keep".text = "";
+        "Videos/.keep".text = "";
+        "work/.keep".text = "";
+        ".face".source = cfg.icon;
+        "Pictures/${
+          cfg.icon.fileName or (builtins.baseNameOf cfg.icon)
+        }".source =
+          cfg.icon;
+      };
+
+      extraOptions = {
+        home.shellAliases = {
+          lc = "${pkgs.colorls}/bin/colorls --sd";
+          lcg = "lc --gs";
+          lcl = "lc -1";
+          lclg = "lc -1 --gs";
+          lcu = "${pkgs.colorls}/bin/colorls -U";
+          lclu = "${pkgs.colorls}/bin/colorls -U -1";
+        };
+
+      };
+    };
+
+    users.users.${cfg.name} =
+      {
+        isNormalUser = true;
+
+        inherit (cfg) name initialPassword;
+
+        home = "/home/${cfg.name}";
+        group = "users";
+
+        shell = pkgs.zsh;
+
+        # Arbitrary user ID to use for the user. Since I only
+        # have a single user on my machines this won't ever collide.
+        # However, if you add multiple users you'll need to change this
+        # so each user has their own unique uid (or leave it out for the
+        # system to select).
+        uid = 1000;
+
+        extraGroups = [ "steamcmd" ] ++ cfg.extraGroups;
+      }
+      // cfg.extraOptions;
+  };
+}

37a0102a945b4a784b1d5106a7121719eda56b89
christoph.hollizeck <christoph.hollizeck@web.de>
2024-03-22 17:28:08 +0100

    Added nix-ld and some comments and nixvim to new flake

--- flake.nix
+++ flake.nix
@@ -19,31 +19,52 @@
       url = "github:snowfallorg/flake";
       inputs.nixpkgs.follows = "unstable";
     };
+
+    # The nix way of configuring vim
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Hardware Configuration
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+
+    # Run unpatched dynamically compiled binaries
+    nix-ld = {
+      url = "github:Mic92/nix-ld";
+      inputs.nixpkgs.follows = "unstable";
+    };
   };
 
-  outputs = inputs: let 
-    lib = inputs.snowfall-lib.mkLib {
-      inherit inputs;
-      src = ./.;
+  outputs = inputs:
+    let
+      lib = inputs.snowfall-lib.mkLib {
+        inherit inputs;
+        src = ./.;
 
-snowfall = {
-        meta = {
-          name = "wyrdgard";
-          title = "Wyrdgard";
+        snowfall = {
+          meta = {
+            name = "wyrdgard";
+            title = "Wyrdgard";
+          };
+
+          namespace = "wyrdgard";
         };
+      };
+    in
+    lib.mkFlake {
 
-        namespace = "wyrdgard";
-      };
-      };
-      in lib.mkFlake {
-      			
       channels-config = { allowUnfree = true; };
 
       outputs-builder = channels: { formatter = channels.nixpkgs.nixpkgs-fmt; };
 
       overlays = with inputs; [
-        # Use the overlay provided by this flake.
         snowfall-flake.overlays.default
+      ];
+
+      systems.modules.nixos = with inputs; [
+        home-manager.nixosModules.home-manager
+        nix-ld.nixosModules.nix-ld
       ];
     };
 }

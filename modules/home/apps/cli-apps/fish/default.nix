6ca1efc75b4d4c7d2976e6bb5dde5d4d61b347da
christoph.hollizeck <christoph.hollizeck@web.de>
2024-03-26 22:10:43 +0100

    starship config in home

--- modules/home/apps/cli-apps/fish/default.nix
+++ modules/home/apps/cli-apps/fish/default.nix
@@ -14,10 +14,42 @@ in {
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      fish
-      starship
-      colorls
-    ];
+    programs = {
+      fish = enabled;
+      starship = {
+        enable = true;
+        enableTransience = true;
+        settings = {
+          character = {
+            success_symbol = "[➜](bold green)";
+            error_symbol = "[✗](bold red) ";
+            vicmd_symbol = "[](bold blue) ";
+          };
+          time = {
+            disabled = false;
+            time_format = "%T";
+            utc_time_offset = "+1";
+          };
+          username = {
+            style_user = "lightgreen";
+            style_root = "red";
+            format = "[$user]($style) ";
+            disabled = false;
+            show_always = true;
+          };
+          hostname = {
+            ssh_only = false;
+            format = "@ [$hostname](bold yellow) ";
+            disabled = false;
+          };
+          directory = {
+            home_symbol = "󰋞 ~";
+            read_only_style = "197";
+            read_only = "  ";
+            format = "at [$path]($style)[$read_only]($read_only_style) ";
+          };
+        };
+      };
+    };
   };
 }

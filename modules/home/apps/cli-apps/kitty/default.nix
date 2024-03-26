f25acf88aca4e82aa7d05f3a60ac46c37d8dfc8c
christoph.hollizeck <christoph.hollizeck@web.de>
2024-03-26 22:56:53 +0100

    update homemanager settings

--- modules/home/apps/cli-apps/kitty/default.nix
+++ modules/home/apps/cli-apps/kitty/default.nix
@@ -16,5 +16,19 @@ in {
     home.packages = with pkgs; [
       kitty
     ];
+
+    programs.kitty = {
+			    enable = true;
+    theme = "Tokyo Night";
+    font = {
+      name = "Code New Roman";
+      size = 15;
+    };
+    shellIntegration.enableFishIntegration = true;
+    settings = {
+      "background_opacity" = "0.9";
+      "shell" = "fish";
+    };
+    		};
   };
 }

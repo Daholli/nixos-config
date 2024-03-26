208e4947a52a5a6371989ded9c937cad605cf717
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-03-26 17:48:15 +0100

    added tide and removed things from the 'main' config to see if it still works

--- modules/nixos/apps/vivaldi/default.nix
+++ modules/nixos/apps/vivaldi/default.nix
@@ -1,17 +1,17 @@
 { options, config, lib, pkgs, ... }:
 with lib;
 with lib.wyrdgard; let
-	cfg = config.wyrdgard.apps.vivaldi;
+  cfg = config.wyrdgard.apps.vivaldi;
 in
 {
-	options.wyrdgard.apps.vivaldi = with types; {
-		enable = mkBoolOpt false "Whether or not to enable vivaldi browser";
-};
+  options.wyrdgard.apps.vivaldi = with types; {
+    enable = mkBoolOpt false "Whether or not to enable vivaldi browser";
+  };
 
-	config = mkIf cfg.enable {
-		environment.systemPackages = with pkgs; [
-			vivaldi
-			vivaldi-ffmpeg-codecs
-];
-};
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      vivaldi
+      vivaldi-ffmpeg-codecs
+    ];
+  };
 }
--- systems/x86_64-linux/workvm/default.nix
+++ systems/x86_64-linux/workvm/default.nix
@@ -1,38 +1,21 @@
-{ pkgs , config , lib, ... }:
+{ pkgs, config, lib, ... }:
 with lib;
 with lib.wyrdgard;
 {
   imports = [ ./hardware.nix ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
   boot.blacklistedKernelModules = [ "hyperv-fb" ];
   virtualisation.hypervGuest.videoMode = "1920x1080";
 
-  users.users.cholli = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-  };
-
   environment.systemPackages = with pkgs; [
-    neovim
-    snowfallorg.flake
-    git
-    gitAndTools.gh
     kitty
     fish
-    vivaldi
+    fishPlugins.tide
 
     fd
     tree
     ripgrep
-
-    nixfmt
   ];
-
-  home-manager.useGlobalPkgs = true;
-
-  networking.networkmanager.enable = true;
 
   services.xserver = {
     enable = true;
@@ -45,27 +28,27 @@ with lib.wyrdgard;
     xkbVariant = "";
   };
 
-	environment.variables.EDITOR = "nvim";
-	environment.variables.SUDOEDITOR = "nvim";
+  environment.variables.EDITOR = "nvim";
+  environment.variables.SUDOEDITOR = "nvim";
 
   # Configure Home-Manager options from NixOS.
   snowfallorg.user.cholli.home.config = {
-	programs.kitty= {
-		theme = "Tokyo Night";
-		shellIntegration.enableFishIntegration = true;
-	};
-};
+    programs.kitty = {
+      theme = "Tokyo Night";
+      shellIntegration.enableFishIntegration = true;
+    };
+  };
 
-	wyrdgard = {
-		apps = {
-			discord = enabled;
-			vivaldi = enabled;
-		};
+  wyrdgard = {
+    apps = {
+      discord = enabled;
+      vivaldi = enabled;
+    };
 
-		submodules = {
-			basics = enabled;
-		};
-	};
+    submodules = {
+      basics = enabled;
+    };
+  };
 
   system.stateVersion = "23.11";
 }

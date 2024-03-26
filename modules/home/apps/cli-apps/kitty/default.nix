d9c99e9afac50f13f38ed21e1c9e426f05df5efe
christoph.hollizeck <christoph.hollizeck@web.de>
2024-03-26 22:43:45 +0100

    update homemanager settings

--- modules/home/apps/cli-apps/kitty/default.nix
+++ modules/home/apps/cli-apps/kitty/default.nix
@@ -8,7 +8,7 @@ with lib;
 with lib.wyrdgard; let
   cfg = config.wyrdgard.apps.cli-apps.kitty;
 in {
-  options.wyrdgard.cli-apps.kitty = {
+  options.wyrdgard.apps.cli-apps.kitty = {
     enable = mkEnableOption "Kity";
   };
 

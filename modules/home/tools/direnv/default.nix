3ccec13d287b4de5a55f6cf4fe8e9e270c53c3b6
christoph.hollizeck <christoph.hollizeck@web.de>
2024-03-26 22:19:05 +0100

    update homemanager settings

--- modules/home/tools/direnv/default.nix
+++ modules/home/tools/direnv/default.nix
@@ -1,24 +1,18 @@
-{
-  options,
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ options, config, lib, pkgs, ... }:
+
 with lib;
-with lib.wyrdgard; let
-  cfg = config.wyrdgard.tools.direnv;
-in {
+with lib.wyrdgard;
+let cfg = config.wyrdgard.tools.direnv;
+in
+{
   options.wyrdgard.tools.direnv = with types; {
     enable = mkBoolOpt false "Whether or not to enable direnv.";
   };
 
   config = mkIf cfg.enable {
-    wyrdgard.home.extraOptions = {
-      programs.direnv = {
-        enable = true;
-        nix-direnv = enabled;
-      };
+    programs.direnv = {
+      enable = true;
+      nix-direnv = enabled;
     };
   };
 }

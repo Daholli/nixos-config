2697a0ef48d08f9cbcecad7c2dbaba0103924670
christoph.hollizeck <christoph.hollizeck@web.de>
2024-03-26 22:33:14 +0100

    update homemanager settings

--- homes/x86_64-linux/cholli@workvm/default.nix
+++ homes/x86_64-linux/cholli@workvm/default.nix
@@ -2,23 +2,20 @@
   lib,
   pkgs,
   config,
+  osConfig ? {},
+  format ? "unknown",
   ...
 }:
-# User information gathered by Snowfall Lib is available.
-let
-  name = config.snowfallorg.user.name;
-  home = config.snowfallorg.user.home.directory;
-in {
-  home = {
-    packages = with pkgs; [neovim];
-
-    sessionVariables = {EDITOR = "nvim";};
-
-    shellAliases = {
-      vim = "nvim";
-      vimdiff = "nvim -d";
+with lib.wyrdgard; {
+  wyrdgard = {
+    apps.cli-apps = {
+      fish = enabled;
+      home-manager = enabled;
     };
 
-    stateVersion = "23.11";
+    tools = {
+      git = enabled;
+      direnv = enabled;
+    };
   };
 }

6c8fc0837e4e4a887c2796ac97cb9af0f843508a
christoph.hollizeck <christoph.hollizeck@web.de>
2024-03-27 22:01:17 +0100

    cutdown on gc time

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -76,8 +76,8 @@ in {
 
       gc = {
         automatic = true;
-        dates = "weekly";
-        options = "--delete-older-than 30d";
+        dates = "daily";
+        options = "--delete-older-than 7d";
       };
 
       # flake-utils-plus

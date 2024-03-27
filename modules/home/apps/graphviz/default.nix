3397f7ea85bc6d1774b125f0f6d7ce1964f43bd5
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-03-27 14:27:19 +0100

    create svg of dependency tree with nixsize

---
+++ modules/home/apps/graphviz/default.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.wyrdgard; let
+  cfg = config.wyrdgard.apps.graphviz;
+in {
+  options.wyrdgard.apps.graphviz = {
+    enable = mkBoolOpt true "Whether or not you want to install graphviz";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      graphviz
+    ];
+
+    home.shellAliases = {
+      nixsize = "nix-du -n=50 | dot -Tsvg > ~/Pictures/store.svg";
+    };
+  };
+}

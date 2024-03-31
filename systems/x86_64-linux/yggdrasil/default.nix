13db4b4d21babbe3e126720a9647a18de5ed440f
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-03-31 19:01:36 +0200

    move nvidia relevant options into one file

--- systems/x86_64-linux/yggdrasil/default.nix
+++ systems/x86_64-linux/yggdrasil/default.nix
@@ -11,22 +11,6 @@ with lib.wyrdgard; {
   environment.systemPackages = with pkgs; [
   ];
 
-  # nvidia
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = false;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-  };
-
   environment.pathsToLink = ["/libexec"];
 
   wyrdgard = {
@@ -40,11 +24,11 @@ with lib.wyrdgard; {
       _1password = enabled;
     };
 
-    system.hardware.bluetooth = enabled;
+    system.hardware = {
+      bluetooth = enabled;
+      gpu.nvidia = enabled;
+    };
   };
-
-  services.xserver.videoDrivers = ["nvidia"];
-  services.xserver.displayManager.sddm.wayland.enable = lib.mkForce false;
 
   system.stateVersion = "23.11";
 }

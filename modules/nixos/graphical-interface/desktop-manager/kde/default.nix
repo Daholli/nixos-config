cdd702a6563208fd3b4a6bf342af87a618363764
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-06-04 22:49:53 +0200

    first attempt at changing the layout and using greetd

---
+++ modules/nixos/graphical-interface/desktop-manager/kde/default.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.wyrdgard;
+let
+  cfg = config.wyrdgard.graphical-interface.desktop-manager.kde;
+in
+{
+  options.wyrdgard.graphical-interface.desktop-manager.kde = with types; {
+    enable = mkEnableOption "Whether to enable a kde plasma6";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ xdg-utils ];
+
+    services = {
+      xserver = enabled;
+      desktopManager.plasma6 = enabled;
+    };
+  };
+}

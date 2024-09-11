0302ec2ac68cd0b46c2dce1ddee96742dac306ec
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-09-12 00:51:57 +0200

    add rofi and set ultrawide as primary display

---
+++ modules/nixos/graphical-interface/desktop-manager/addons/rofi/default.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  namespace,
+  options,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.${namespace}.graphical-interface.desktop-manager.addons.rofi;
+
+  inherit (lib) mkIf mkEnableOption mkOption types;
+in
+{
+    options.${namespace}.graphical-interface.desktop-manager.addons.rofi = {
+    enable = mkEnableOption "Rofi";
+    package = mkOption {
+      type = types.package;
+      default = pkgs.rofi;
+      description = "The package to use for Rofi";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
+    wyrdgard.home.file = {
+      ".config/rofi/config.rasi".source = ./config.rasi;
+    };
+  };
+}

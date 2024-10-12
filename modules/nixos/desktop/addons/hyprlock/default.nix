57b06c12f2b2a15ceb05c5dd87282a01230a0050
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-10-12 14:15:33 +0200

    hyprlock: update config to include image
    also include default startup option without nividia shenanigas for hyprland

--- modules/nixos/desktop/addons/hyprlock/default.nix
+++ modules/nixos/desktop/addons/hyprlock/default.nix
@@ -9,7 +9,6 @@
 }:
 let
   inherit (lib) mkIf mkEnableOption;
-  inherit (lib.${namespace}) enabled;
 
   hyprlock-package = inputs.hyprlock.packages.${system}.hyprlock;
 
@@ -21,6 +20,8 @@ let
   '';
 
   cfg = config.${namespace}.desktop.addons.hyprlock;
+
+  username = config.${namespace}.user.name;
 in
 {
   options.${namespace}.desktop.addons.hyprlock = {
@@ -36,18 +37,18 @@ in
       ];
     };
 
-    snowfallorg.users.${config.${namespace}.user.name}.home.config = {
+    snowfallorg.users.${username}.home.config = {
 
       programs.hyprlock = {
         enable = true;
         package = hyprlock-package;
         settings = {
+          # inspiration from https://github.com/justinmdickey/publicdots/blob/main/.config/hypr/hyprlock.conf
           background = [
             {
               monitor = "DP-2";
               path = "/tmp/screenshot1.png";
 
-              # all these options are taken from hyprland, see https://wiki.hyprland.org/Configuring/Variables/#blur for explanations
               blur_passes = 1; # 0 disables blurring
               blur_size = 7;
               noise = 1.17e-2;
@@ -56,32 +57,11 @@ in
               monitor = "HDMI-A-1";
               path = "/tmp/screenshot2.png";
 
-              # all these options are taken from hyprland, see https://wiki.hyprland.org/Configuring/Variables/#blur for explanations
               blur_passes = 2; # 0 disables blurring
               blur_size = 7;
               noise = 1.17e-2;
             }
           ];
-
-          input-field = {
-            monitor = "DP-2";
-            size = "200,50";
-            outline_thickness = 2;
-            dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
-            dots_spacing = 0.35; # Scale of dots' absolute size, 0.0 - 1.0
-            dots_center = true;
-            outer_color = "rgba(0, 0, 0, 0)";
-            inner_color = "rgba(0, 0, 0, 0.2)";
-            font_color = "rgb(255,129,0)";
-            fade_on_empty = false;
-            rounding = -1;
-            check_color = "rgb(204, 136, 34)";
-            placeholder_text = ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
-            hide_input = false;
-            position = "0, -100";
-            halign = "center";
-            valign = "center";
-          };
 
           label = [
             {
@@ -107,19 +87,37 @@ in
 
             }
           ];
+
+          image = {
+            monitor = "DP-2";
+            path = "/home/${username}/Pictures/profile.png";
+
+            position = "0, 50";
+            halign = "center";
+            valign = "center";
+          };
+
+          input-field = {
+            monitor = "DP-2";
+            size = "200,50";
+            outline_thickness = 2;
+            dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
+            dots_spacing = 0.35; # Scale of dots' absolute size, 0.0 - 1.0
+            dots_center = true;
+            outer_color = "rgba(0, 0, 0, 0)";
+            inner_color = "rgba(0, 0, 0, 0.2)";
+            font_color = "rgb(111, 45, 104)";
+            fade_on_empty = false;
+            rounding = -1;
+            check_color = "rgb(30, 107, 204)";
+            placeholder_text = ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
+            hide_input = false;
+            position = "0, -100";
+            halign = "center";
+            valign = "center";
+          };
         };
       };
     };
-
-    # ${namespace}.home.extraOptions = {
-    #   programs.hello = enabled;
-
-    #   programs.hyprlock = {
-    #     enable = true;
-    #     settings = {
-    #       monitor = "DP-2";
-    #     };
-    #   };
-    # };
   };
 }

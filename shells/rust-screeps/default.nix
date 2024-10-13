40facdc4ea5542ad49c0ee789a1691b1225d5db2
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-10-13 17:01:33 +0200

    shells: make barebones rust shell and create additional screeps shell

--- shells/rust-screeps/default.nix
+++ shells/rust-screeps/default.nix
@@ -16,11 +16,20 @@
   ...
 }:
 let
-  fenix = inputs.fenix.packages.${system}.latest.toolchain;
+  fenix =
+    with inputs.fenix.packages.${system};
+    combine [
+      latest.toolchain
+      targets.wasm32-unknown-unknown.latest.rust-std
+    ];
 in
 mkShell {
   # Create your shell
   nativeBuildInputs = [
     fenix
+    pkgs.llvmPackages.bintools
+    pkgs.wasm-pack
   ];
+
+  CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
 }

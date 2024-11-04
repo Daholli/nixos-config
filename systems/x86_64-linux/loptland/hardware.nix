b421c3de6856939abca158ee40f57611bd881f3f
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-11-04 21:23:24 +0100

    introduce new host

---
+++ systems/x86_64-linux/loptland/hardware.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
+
+{
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "ata_piix"
+    "uhci_hcd"
+    "virtio_pci"
+    "sr_mod"
+    "virtio_blk"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  swapDevices = [ ];
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostplatform = lib.mkDefault "x86_64-linux";
+}

8b78c651b66d1b75339bf61e3753734234c9452d
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-11-20 05:54:14 +0100

    nix: enable community cachix by default

--- modules/nixos/submodules/basics/default.nix
+++ modules/nixos/submodules/basics/default.nix
@@ -33,7 +33,14 @@ in
     ];
 
     ${namespace} = {
-      nix = enabled;
+      nix = {
+        enable = true;
+        extra-substituters = {
+          "https://nix-community.cachix.org" = {
+            key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+          };
+        };
+      };
 
       apps.cli-apps.helix = enabled;
 

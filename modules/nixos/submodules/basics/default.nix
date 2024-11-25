965aa569ffa8f0f0997ee0dc64693b6a05f6cdfb
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-11-25 15:52:10 +0100

    rp5: finally working nix-sdcard, enableing remote build on the nix system now

--- modules/nixos/submodules/basics/default.nix
+++ modules/nixos/submodules/basics/default.nix
@@ -36,6 +36,9 @@ in
       nix = {
         enable = true;
         extra-substituters = {
+          "https://cache.lix.systems" = {
+            key = "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o=";
+          };
           "https://nix-community.cachix.org" = {
             key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
           };

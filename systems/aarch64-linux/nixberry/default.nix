047d4f7dbda02ea3c40211d7d0ece030cb43bf40
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-11-25 22:25:05 +0100

    rp5: working adguardhome!

--- systems/aarch64-linux/nixberry/default.nix
+++ systems/aarch64-linux/nixberry/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   inputs,
   lib,
   modulesPath,
@@ -9,6 +10,8 @@
 with lib.${namespace};
 let
   inherit (lib) mkForce;
+
+  ipAddress = "192.168.178.2";
 in
 {
   imports = with inputs.nixos-hardware.nixosModules; [
@@ -16,6 +19,7 @@ in
     raspberry-pi-5
   ];
 
+  security.sudo.wheelNeedsPassword = false;
   users.users.remotebuild = {
     isNormalUser = true;
     createHome = false;
@@ -51,10 +55,11 @@ in
     interfaces.wlan0 = {
       ipv4.addresses = [
         {
-          address = "192.168.178.2";
+          address = ipAddress;
           prefixLength = 24;
         }
       ];
+      useDHCP = true;
     };
     defaultGateway = {
       address = "192.168.178.1";
@@ -68,6 +73,59 @@ in
           pskRaw = "521b6d766b27276c29c7b6bec5b495b1c52bf88b0682277e65b37dc649b630de";
         };
       };
+    };
+  };
+
+  networking.firewall = {
+    allowedTCPPorts = [
+      53
+      80
+    ];
+    allowedUDPPorts = [
+      53
+    ];
+  };
+
+  services.adguardhome = {
+    enable = true;
+    host = ipAddress;
+    port = 80;
+
+    settings = {
+      http = {
+        address = "0.0.0.0:80";
+      };
+      dns = {
+        ratelimit = 0;
+        bind_hosts = [ "0.0.0.0" ];
+        upstream_dns = [
+          "1.1.1.1"
+          "1.0.0.1"
+          "8.8.8.8"
+          "8.8.4.4"
+        ];
+      };
+      filtering = {
+        protection_enabled = true;
+        filtering_enabled = true;
+      };
+
+      filters =
+        map
+          (url: {
+            enabled = true;
+            url = url;
+          })
+          [
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt" # AdGuard Dns filter
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_59.txt" # AdGuard Dns PopupHosts filter
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt" # The Big List of Hacked Malware Web Sites
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt" # malicious url blocklist
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_18.txt" # Phishing
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_24.txt"
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_47.txt"
+          ];
+
     };
   };
 

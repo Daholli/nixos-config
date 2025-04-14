8216ce2560d89bdb8dadcfd173c30313c102fdfa
Christoph Hollizeck <christoph.hollizeck@hey.com>
2025-04-14 18:24:15 +0200

    yggdrasil: move hyprland config to separate file to clean up

--- systems/x86_64-linux/yggdrasil/default.nix
+++ systems/x86_64-linux/yggdrasil/default.nix
@@ -9,7 +9,10 @@ let
   inherit (lib.${namespace}) enabled disabled;
 in
 {
-  imports = [ ./hardware.nix ];
+  imports = [
+    ./hardware.nix
+    ./hyprland_config.nix
+  ];
 
   environment.systemPackages = with pkgs; [
     path-of-building
@@ -71,45 +74,6 @@ in
 
     desktop.hyprland = {
       enable = true;
-      settings = {
-        monitor = [
-          #Ultrawide
-          "DP-2,3440x1440@144, 0x0, 1"
-          #Vertical
-          "HDMI-A-1, 1920x1080@144, auto-right, 1, transform, 1"
-          # nividia kernel issues
-          "Unknown-1, disable"
-        ];
-
-        cursor = {
-          no_hardware_cursors = true;
-        };
-
-        exec-once = [
-          "[workspace 1 silent] obsidian --disabled-gpu"
-          "[workspace 9 silent] git-butler"
-
-          "${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --primary"
-        ];
-
-        workspace = [
-          "1, monitor:DP-2"
-          "2, monitor:DP-2"
-          "3, monitor:DP-2"
-          "4, monitor:DP-2"
-          "5, monitor:DP-2"
-          "6, monitor:DP-2"
-          "7, monitor:HDMI-A-1"
-          "8, monitor:HDMI-A-1"
-          "9, monitor:HDMI-A-1"
-        ];
-
-        windowrulev2 = [
-          "workspace 8 silent, class:^(steam)$, title:^(Friends List)"
-          "workspace 8 silent, class:^(vesktop)$, title:^(Discord)"
-          "workspace 7 silent, class:^(com.obsproject.Studio)$"
-        ];
-      };
     };
 
     services = {

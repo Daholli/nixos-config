bef0142b8fd7e70648bfd03d98bdfb5617a5c39e
Christoph Hollizeck <christoph.hollizeck@hey.com>
2025-06-12 08:15:44 +0200

    hydra: limit devshells to only main architecture

--- flake.nix
+++ flake.nix
@@ -170,7 +170,7 @@
       hydraJobs = {
         hosts = lib.mapAttrs (_: cfg: cfg.config.system.build.toplevel) self.outputs.nixosConfigurations;
         packages = self.packages;
-        shells = self.devShells;
+        shells = lib.filterAttrs (name: shell: name == "x86_64-linux") self.devShells;
       };
     };
 }

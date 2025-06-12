2db7f29de5ec87c2fb0f586533f5d8e6991b6029
Christoph Hollizeck <christoph.hollizeck@hey.com>
2025-06-12 22:36:42 +0200

    pob: make sure it runs with the proper QT_QPA_PLATFORM

--- overlays/pathofbuilding/default.nix
+++ overlays/pathofbuilding/default.nix
@@ -1,3 +1,8 @@
-{ channels, ... }:
-
-final: prev: { inherit (channels.nixpkgs-unstable) pobfrontend; }
+{ ... }:
+final: prev: {
+  path-of-building = prev.path-of-building.overrideAttrs (prevAttrs: {
+    postInstall = ''
+      wrapProgram $out/bin/pobfrontend --set QT_QPA_PLATFORM xcb
+    '';
+  });
+}

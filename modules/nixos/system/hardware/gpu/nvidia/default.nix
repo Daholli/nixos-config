08335f24a06a6f59c5ca594f7ce231dacc9b793f
Christoph Hollizeck <christoph.hollizeck@hey.com>
2025-06-13 14:46:39 +0200

    nvidia: use open driver

--- modules/nixos/system/hardware/gpu/nvidia/default.nix
+++ modules/nixos/system/hardware/gpu/nvidia/default.nix
@@ -26,7 +26,7 @@ in
     hardware.nvidia = {
       modesetting.enable = true;
       powerManagement.enable = true;
-      open = false;
+      open = true;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.beta; # stable, beta
     };

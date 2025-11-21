ba7f7b88cc27b292e22757f590c6dd281a21b1f3
Christoph Hollizeck <christoph.hollizeck@hey.com>
2025-11-21 19:34:34 +0100

    helix: add cache

--- modules/base/system/nixdaemon.nix
+++ modules/base/system/nixdaemon.nix
@@ -57,18 +57,20 @@
             substituters = [
               "https://cache.lix.systems"
               "https://nix-community.cachix.org"
-              "https://nixcache.christophhollizeck.dev"
+              "https://helix.cachix.org"
               "https://hyprland.cachix.org"
               "https://nix-gaming.cachix.org"
               "https://nixos-raspberrypi.cachix.org"
+              "https://nixcache.christophhollizeck.dev"
             ];
             trusted-public-keys = [
               "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
               "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-              "christophhollizeck.dev:7pPAvm9xqFQB8FDApVNL6Tii1Jsv+Sj/LjEIkdeGhbA="
+              "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
               "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
               "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
               "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+              "christophhollizeck.dev:7pPAvm9xqFQB8FDApVNL6Tii1Jsv+Sj/LjEIkdeGhbA="
             ];
           }
           // (lib.optionalAttrs config.programs.direnv.enable {

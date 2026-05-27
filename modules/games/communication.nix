210af2a7bfe3e923dd48582cb92f0538c932f8ae
Christoph Hollizeck <christoph.hollizeck@hey.com>
2026-05-27 21:35:23 +0200

    obs: update configuration

--- modules/games/communication.nix
+++ modules/games/communication.nix
@@ -11,8 +11,10 @@
       programs.obs-studio = {
         enable = true;
         plugins = with pkgs.obs-studio-plugins; [
-          wlrobs
           obs-move-transition
+          obs-pipewire-audio-capture
+          input-overlay
+          obs-vaapi
         ];
       };
     };
